4424299a7b1d1991e1804f61c6a4aae457714a62
robson488 <00001106734099sp@al.educacao.sp.gov.br>
2024-12-02 08:08:11 -0300

    envio parcial do projeto de algoritimo

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorAll('.btn-proximo');  
+
+avanca.forEach(button => { 
+     button.addEventListener*('click,funcition'()} 
+     const atual document.querySelector('.ativo');  
+     const proximoPasso = 'passo-' + this.getAttribute('data- próximo'); 
+
+     atual.classList.remove('ativo'); 
+     document.getElementById(proximoPasso).classList.add('ativo'); 
+ }) 
+})  
